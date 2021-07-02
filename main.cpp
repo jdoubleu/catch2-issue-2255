94e05f3c9b91132b60a203c102b11c493e2fde3e
jdoubleu <hi@jdoubleu.de>
2021-07-02 21:32:09 +0200

    install Catch2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,2 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch2/catch.hpp>
